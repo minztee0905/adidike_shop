050a2c103723ae36bbcab30fa1c9d2f02e1ec687
Tran Quoc Vy <quocvy23072005@gmail.com>
2024-07-22 19:33:31 +0700

    VyTQ fix size

--- adidike_shop/adidike_shop/product.cs
+++ adidike_shop/adidike_shop/product.cs
@@ -208,5 +208,15 @@ namespace adidike_shop
         {
 
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
